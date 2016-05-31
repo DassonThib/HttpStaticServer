a0586bb417c71ee4549665f9d03bb9ba881426d8
Stitchou <la_muerte_97216@hotmail.com>
2016-05-31 15:55:49 +0200

    commit forcé

--- src/Classes/HttpRequest.java
+++ src/Classes/HttpRequest.java
@@ -1,10 +1,11 @@
 package Classes;
 import Interface.IHttpRequest;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -19,21 +20,26 @@ public class HttpRequest implements IHttpRequest {
     private Map<String, String> parameters;
     private String method;
     private String relativePath, absolutePath;
+    private BufferedReader br;
+    private boolean upload;
 
     public HttpRequest(Socket s) throws IOException{
+        int i = 0;
+        upload = false;
         this.cookies = new HashMap<>();
         this.parameters = new HashMap<>();
         this.socket = s;
-        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         String str = br.readLine();
         if(str != null) {
             String[] tab = str.split(" / ");
             this.method = tab[0];
+
             System.out.println(method);
             String[] param;
             do {
+                System.out.println(str);
                 if ((param = str.split(":")).length > 1) {
-                    System.out.println(str);
                     this.parameters.put(param[0], param[1]);
                     if (param[0].equals("Host")){
                         String[] path;
@@ -43,12 +49,28 @@ public class HttpRequest implements IHttpRequest {
                             this.absolutePath = "";
                         }
                     }
+                    if (param[0].equals("Content-Type"))
+                        i++;
+                    if (param[0].equals("Content-Type") && param[1].contains("multipart/form-data")){
+                        String[] tmp = param[1].split(";");
+                        String [] tmp2 = tmp[1].split("=");
+                        parameters.put(tmp2[0],tmp2[1]);
+                        upload = true;
+                    }
+                    if (param[0].equals("Content-Disposition") && param[1].contains("form-data")){
+                        String[] tmp = param[1].split(";");
+                        String [] tmp2 = tmp[2].split("=");
+                        parameters.put(tmp2[0],tmp2[1].replace("\"",""));
+                    }
                     //System.out.println("Clé : " + param[0] + " ,Valeur : " + param[1]);
                 }
-                if (str.equals("")) {
-                    break;
-                }
-            } while ((str = br.readLine()) != null);
+                if (str.equals("") && i > 1 )
+                   // if( (br.readLine() == null)) {
+                        break;
+               // }
+            } while ((str = br.readLine()) != null );
+            if(method.contains("POST") && i >1)
+                doPost();
         }
     }
 
@@ -93,5 +115,47 @@ public class HttpRequest implements IHttpRequest {
     @Override
     public String getAbsolutePath() {
         return "C:\\www\\monsite\\"+this.absolutePath;
+    }
+
+    public void doPost(){
+        String str = "";
+
+        File yourFile = new File(getAbsolutePath()+"\\files\\"+(String)getParameter(" filename"));
+        if(!yourFile.exists()) {
+            try {
+                yourFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        FileOutputStream outputStream = null;
+        try {
+             outputStream =
+                    new FileOutputStream(yourFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            br.readLine();
+            while((str = br.readLine()) != null)
+            {
+                System.out.println(str);
+                if(str.contains((String)getParameter(" boundary")))
+                    break;
+                str+= "\r\n";
+                outputStream.write(str.getBytes());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
