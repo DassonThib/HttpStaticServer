2c2b48aa7b7ec6d3f74775abb8d8d3e52547091a
Thibault <dassonville.thibault@gmail.com>
2016-06-01 11:25:56 +0200

    thread vuzy

--- src/Thread/Worker.java
+++ src/Thread/Worker.java
@@ -3,38 +3,21 @@ package Thread;
 /**
  * Created by Thibault on 31/05/2016.
  */
-public class Worker extends Thread {
+public class Worker implements Runnable {
 
-    private Job job;
-    private String command;
+    ThreadPool threadPool;
 
-    public Worker(Job j, String c){
-        this.job = j;
-        this.command = c;
+    public Worker(ThreadPool threadPool) {
+        this.threadPool = threadPool;
     }
 
     @Override
     public void run() {
-        if (this.job != null && this.command != null) {
-            System.out.println(Thread.currentThread().getName() + " Start. Command = " + this.command);
-            job.execute();
-            System.out.println(Thread.currentThread().getName() + " end.");
+        while (!threadPool.shouldStop) {
+            Action action = threadPool.consume();
+            if (action != null) {
+                action.apply();
+            }
         }
-    }
-
-    public Job getJob(){
-        return this.job;
-    }
-
-    public void setJob(Job j){
-        this.job = j;
-    }
-
-    public String getCommand(){
-        return this.command;
-    }
-
-    public void setCommand(String c){
-        this.command = c;
     }
 }
